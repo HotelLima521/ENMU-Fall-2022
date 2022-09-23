8c654473c80abc5529389c8cc41873fed3010bbf
Derek White <white.derek26@pm.me>
2022-09-23 12:39:41 -0600

    Update lab, added in base10 to base60 converter to simplify shit

--- COMPSCI/java/Labs/Lab2/Lab2b.java
+++ COMPSCI/java/Labs/Lab2/Lab2b.java
@@ -1,20 +1,43 @@
 import java.util.Scanner;
 public class Lab2b {
+	public static int base10to60(int x){
+		int time = x;
+		int[] q;
+		int[] r;
+		q[0] = time;
+		int i = 0;
+		while(q[i] > 0) {
+			r[i] = q[i] % 60;
+			q[i+1] = q[i] / 60;
+			i+=1;
+		}
+		return time;
+	}
 	public static void counter(int a, int b){
 		int hours = 0;
 		int minutes = 0;
 		int time1 = a;
 		int time2 = b;
+		int count;
+		// Really need to convert from base 10 to base 60 here
+		time1 = base10to60(time1);
+		time2 = base10to60(time2);
+		// Okay, conversion complete, now need to compare the numbers.
+		count = time1;
 		while(time1 != time2){
-			time1++;
-			minutes++;
-			if(minutes == 60){
-				hours++;
-				minutes = 0; // Resetting minutes to 0 due to increasing hour.
+			while (time1 > time2){
+				time1++;
+				count++;
+				if(time1 == base10to60(2400)){
+					time1=0;
+				}
 			}
-			if(time1 == 2400){
-				time1 = 0; // Resetting time to 0 due to day change.
-			}
+			int hours1 = count/60;
+			int hours2 = time2/60;
+			hours = hours2 - hours1;
+			int minutes1 = count%60;
+			int minutes2 = time2%60;
+			minutes = minutes2 - minutes1;
 			// Debugging purposes only
 			System.out.println("The time difference is " + hours + " hour(s) and " + minutes + " minutes.");
 			}
@@ -39,13 +62,13 @@ public class Lab2b {
 		 * ### May not have to have multiple if statements here tbh ###
 		 * ############################################################
 		 */
-		if(time1 < time2){ // Time1 is earlier than Time2
+		if(time1 != time2){ // Time1 is earlier than Time2
 			counter(time1, time2);			
 
 		}
-		else if(time1 > time2){ // Time1 is later than Time2, must add additional hours and minutes to catch up since time2 is in the next day
-			counter(time1, time2);
-		}
+		//else if(time1 > time2){ // Time1 is later than Time2, must add additional hours and minutes to catch up since time2 is in the next day
+		//	counter(time1, time2);
+		//}
 		else{ // Time 1 and 2 are the same
 		      System.out.println("Both times are the same, 0 hour and 0 minute differences");
 		}
