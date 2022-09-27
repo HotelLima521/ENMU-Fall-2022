831d6815be5d97e6314bbac6dcc72855e07ff635
Derek White <white.derek26@pm.me>
2022-09-27 11:53:40 -0600

    Add Lab2b fix

--- COMPSCI/java/Labs/Lab2/Lab2b.java
+++ COMPSCI/java/Labs/Lab2/Lab2b.java
@@ -1,84 +1,27 @@
 import java.util.Scanner;
 public class Lab2b {
-	public static int base10to60(int x){
-		int time = x;
-		int[] q;
-		int[] r;
-		q[0] = time;
-		int i = 0;
-		while(q[i] > 0) {
-			r[i] = q[i] % 60;
-			q[i+1] = q[i] / 60;
-			i+=1;
-		}
-		return time;
-	}
-	public static void counter(int a, int b){
+		public static void compute(int t1, int t2){
 		int hours = 0;
 		int minutes = 0;
-		int time1 = a;
-		int time2 = b;
-		int count;
-		// Really need to convert from base 10 to base 60 here
-		time1 = base10to60(time1);
-		time2 = base10to60(time2);
-		// Okay, conversion complete, now need to compare the numbers.
-		count = time1;
-		while(time1 != time2){
-			while (time1 > time2){
-				time1++;
-				count++;
-				if(time1 == base10to60(2400)){
-					time1=0;
-				}
+		int time = 0;
+		int time1 = t1;
+		int time2 = t2;
+			if(time1 < time2){
+				time = time2 - time1;
 			}
-			int hours1 = count/60;
-			int hours2 = time2/60;
-			hours = hours2 - hours1;
-			int minutes1 = count%60;
-			int minutes2 = time2%60;
-			minutes = minutes2 - minutes1;
-			// Debugging purposes only
-			System.out.println("The time difference is " + hours + " hour(s) and " + minutes + " minutes.");
+			else if(time1 > time2){
+				time = (time2-(time1-1200))+1200;
 			}
-		//System.out.println("The time difference is " + hours + " hour(s) and " + minutes + " minutes.");
-	}
-	public static void compute(int a, int b){
-		/*
-		 * May have to split hours and minutes. E.g. 0100 will split into 01 and 00 so they can be worked on
-		 * individually. Then will be combined as ints 0100 + 0000 later on
-		 * 
-		 * Additionally, convert to base 60, so it's easier to work with. e.g.:
-		 * 1....59 ->
-		 * 100...159 ->
-		 * 200...259 ->
-		 * 300
-		 *
-		 * OR just parse string input into two pieces and compare the two; should be easiest route.
-		 */
-		int time1 = a;
-		int time2 = b;
-		/* ############################################################
-		 * ### May not have to have multiple if statements here tbh ###
-		 * ############################################################
-		 */
-		if(time1 != time2){ // Time1 is earlier than Time2
-			counter(time1, time2);			
-
+		hours = time/100;
+		minutes = time%100;
+		System.out.println("The time difference is " + hours + " hours, and " + minutes + " minutes.");
 		}
-		//else if(time1 > time2){ // Time1 is later than Time2, must add additional hours and minutes to catch up since time2 is in the next day
-		//	counter(time1, time2);
-		//}
-		else{ // Time 1 and 2 are the same
-		      System.out.println("Both times are the same, 0 hour and 0 minute differences");
-		}
-	}
 	public static void main(String[] args){
-		Scanner userinput = new Scanner(System.in);
+		Scanner userinput = new Scanner(System.in); // Setting up user input due to goofy Java Syntax
 		System.out.println("Please enter first time in 24-hour format\n-> ");
-		int time1 = userinput.nextInt(10); // Enforcing base 10
+		int time1 = userinput.nextInt(10); // Enforcing base 10, not really necessary to do this, but it's good practice
 		System.out.println("Please enter second time in 24-hour format\n-> ");
-		int time2 = userinput.nextInt(10); // Enforcing base 10
+		int time2 = userinput.nextInt(10); // Enforcing base 10, not really necessary to do this, but it's good practice.
 		compute(time1, time2);
 
 	}
---
+++ Spanish/amigos.com.md
@@ -0,0 +1,11 @@
+Hola, me llamo es Derek, Soy es alto y delgado. Me gusta la electronica y el mechanico. Yo tengo dos perros y treinta pollos. Me gusta conduciendo rapido en mi volkswagen tambien. Me gusta mucho el cafe!
+
+Natalia es similar a venada porque el maquillaje. Natalia es feliz tambien. Ella esta en un bosque. Ella es un costa rica posiblemente?
+
+1. La fecha de hoy es bienty dos septiembre
+2. Mi cumpleanos es bienty seis marzon
+3. tengo clase de espanol en martes y jueves
+4. Tengo no tienes clase en mayo, junio, julio, y deciembre
+5. No me gusta estudiar en el verano porque muy caliente
+
+
